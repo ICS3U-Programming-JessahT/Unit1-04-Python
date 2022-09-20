9c4511921e69e4b5e663ab3090a5da3da2fdb538
Jessah Tumbaga <jessah.tumbaga@stu.ocsb.ca>
2022-09-20 07:38:05 +0000

    Added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created By: Jessah
+# Date: Sept 19, 2022
+# This program calculates the area and perimeter of a rectangle.
+
+
+def main():
+    print("If the rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(5 * 3))
+    print("Perimeter is {}cm".format(2 * (5 + 3)))
+
+
+if __name__ == "__main__":
+    main()
